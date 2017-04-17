b2402ad55273401200e2fe5d68edf15d56135440
TerenceFeng <tyxfeng@gmail.com>
2017-04-17 15:45:14 -0400

    multiple recipes

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,26 @@ router.get("/", (req, res) => {
 });
 
 router.get("/test", (req, res) => {
-	/*
-	 * res.render("cards/card_color", {
-	 *     backgroundColor: "orange",
-	 *     category: "Mideast",
-	 *     link: "#",
-	 *     title: "Qatar Special",
-	 *     description: "..."
-	 * });
-	 */
-	res.render("cards/card_half_pic_half_color.handlebars", {
-		backgroundColor: "black",
-		backgroundPicPath: "/public/img/city-1.jpg",
-		category: "Mediterranean",
-		title: "Mediterranean",
-		link: "http://www.foodandwine.com/recipes/mediterranean-pink-lady",
-		description: "Angus Winchester loves limoncello and Campari and wanted to combine them in a classic-style (that is, not overly esoteric or fussy) cocktail. The result is a pretty pink drink that's citrusy and crisp."
-	});
+	let recipes = [
+		{	backgroundColor: "black",
+			backgroundPicPath: "/public/img/city-1.jpg",
+			category: "Mediterranean",
+			title: "Mediterranean",
+			link: "http://www.foodandwine.com/recipes/mediterranean-pink-lady",
+			description: "Angus Winchester loves limoncello and Campari and wanted to combine them in a classic-style (that is, not overly esoteric or fussy) cocktail. The result is a pretty pink drink that's citrusy and crisp."
+		},
+		{	backgroundColor: "black",
+			backgroundPicPath: "/public/img/city-1.jpg",
+			category: "Mediterranean",
+			title: "Mediterranean",
+			link: "http://www.foodandwine.com/recipes/mediterranean-pink-lady",
+			description: "Angus Winchester loves limoncello and Campari and wanted to combine them in a classic-style (that is, not overly esoteric or fussy) cocktail. The result is a pretty pink drink that's citrusy and crisp."
+		}
 
+	];
+	res.render("cards/card.handlebars", {
+		recipes: recipes
+	});
 
 });
 
